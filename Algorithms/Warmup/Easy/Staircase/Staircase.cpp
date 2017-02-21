17a9045f8e67811c03e78724f2a71e0db4dd0700
Taejung Chang <tchang@zulily.com>
2017-02-20 21:16:56 -0800

    Save unsaved

--- Algorithms/Warmup/Easy/Staircase/Staircase.cpp
+++ Algorithms/Warmup/Easy/Staircase/Staircase.cpp
@@ -11,9 +11,16 @@ int main() {
     cin >> n;
     
     for(int i = 0; i < n; ++i) {
+      int secDiagonalIndex = n - i - 1;
       for(int j = 0; j < n; ++j) {
-        
+        if (j < secDiagonalIndex) {
+          cout << ' ';
+        }
+        else {
+          cout << '#';
+        }
       }
+      cout << endl;
     }   
     return 0;
 }
